2f49af5b4932ab2e189db89c24564908fa11d38f
wangjianan <wangjianan@weshare.com.cn>
2015-11-24 19:56:37 +0800

    add auto create code and sql branch.

---
+++ Tool/sql/migrations/V2__core_userinfo.sql
@@ -0,0 +1,27 @@
+/*
+Navicat MariaDB Data Transfer
+
+Source Server         : local
+Source Server Version : 50544
+Source Host           : 127.0.0.1:3306
+Source Database       : storm
+
+Target Server Type    : MariaDB
+Target Server Version : 50544
+File Encoding         : 65001
+
+Date: 2015-11-24 17:36:35
+*/
+
+-- ----------------------------
+-- Table structure for user_info
+-- ----------------------------
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL DEFAULT 0 COMMENT '姓名',
+  `email` VARCHAR(50) NOT NULL DEFAULT 0 COMMENT '邮箱',
+  `phone` VARCHAR(50) NOT NULL DEFAULT 0 COMMENT '手机',
+  `create_time` int(11) NOT NULL DEFAULT 0 COMMENT '创建日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='人员表';
---
+++ src/main/java/core/com/Application.java
@@ -0,0 +1,14 @@
+package core.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by WangJianan on 2015/11/24.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
