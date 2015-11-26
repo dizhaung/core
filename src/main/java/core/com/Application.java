dfa19b1cc1bff8cb892559ae9fe046e4a7b0e243
wangjianan <wangjianan@weshare.com.cn>
2015-11-26 18:32:50 +0800

    fix

--- src/main/java/core/com/Application.java
+++ src/main/java/core/com/Application.java
@@ -2,17 +2,15 @@ package core.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by WangJianan on 2015/11/24.
  */
 @SpringBootApplication
+@ImportResource({"classpath*:config/spring.xml"})
 public class Application {
-    @RequestMapping("/")
-    public String greeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/core/com/control/UserControl.java
+++ src/main/java/core/com/control/UserControl.java
@@ -1,7 +1,12 @@
 package core.com.control;
 
+import core.com.dao.UserInfoMapper;
+import core.com.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by WangJianan on 2015/11/25.
@@ -9,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserControl {
 
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
     @RequestMapping("index")
-    public String toIndex(){
-        return "Hello World";
+    public String toIndex() {
+        List<UserInfo> userInfoList = userInfoMapper.selectByExample(null);
+        return "Hello World：" + userInfoList.size();
     }
 }
